bb8f252296dcf2b76b698b5d9ad00930f6d2814d
Mouaffakkub <36240791+Mouaffakkub@users.noreply.github.com>
2018-04-11 18:10:35 -0600

    Initial commit

---
+++ main.lua
@@ -0,0 +1,41 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+local ninja = display.newImageRect("./assets/sprites/Ninja Boi.png", 700, 900)
+ninja.x = 600
+ninja.y = display.contentHeight - 350 
+ninja.ID = "ninja"
+
+
+for loopCounter = 1, 50 do 
+    print(loopCounter) 
+    ninja.x = ninja.x + 10
+end
+
+local ghost = display.newImageRect("./assets/sprites/ghost.png", 500, 700)
+ghost.x = 350
+ghost.y = display.contentHeight - 350 
+ghost.ID = "ghost"
+
+local function ghostTouch( event )
+
+    ghost.alpha = 0.0
+    return true
+end
+
+ghost:addEventListener( "touch", ghostTouch )
+
+local lightning = display.newImageRect("./assets/sprites/Lightning.png", 700, 700)
+lightning.x = 1700
+lightning.y = display.contentHeight - 1100
+lightning.ID = "lightning"
+
+local function lightningTouch( event )
+
+
+    return true
+end
+
+lightning:addEventListener( "touch", lightningTouch )
